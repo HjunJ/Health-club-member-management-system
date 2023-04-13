6cf73e97057130c3a55ccd97c508c628d308c1ea
Jeong-Hyeokjun <wnsdl1010200@naver.com>
2023-04-14 03:45:03 +0900

    first repository had a conflicted error, so changed to new repository.

--- xmas/src/xmas/MenuManager2.java
+++ xmas/src/xmas/MenuManager2.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class MenuManager2 {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
+		Scanner input = new Scanner(System.in); 
 		
 		ArrayList<MemberInfo> memberList = new ArrayList<MemberInfo>();
 		int NumAccept = 0;
